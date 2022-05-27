1014d32285705ae27a4e9e583858177610f8169c
fesper-s <fafaeesfabricio@gmail.com>
2022-05-27 14:42:53 -0300

    added get_next_line.c and get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/27 12:27:17 by fesper-s          #+#    #+#             */
+/*   Updated: 2022/05/27 12:30:41 by fesper-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
