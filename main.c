29cc0d0b2b127d3fd83e9fe65a964fc0c2fe1c03
fesper-s <fafaeesfabricio@gmail.com>
2022-06-03 14:26:08 -0300

    modified get_next_line.c and main.c

--- main.c
+++ main.c
@@ -6,21 +6,26 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 08:27:07 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/06/02 14:52:11 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/06/03 09:37:52 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int	main(void)
 {
-	int	fd;
+	int		fd;
 	char	*str;
+	char	*str2;
 
 	fd = open("./teste.txt", O_RDONLY);
 	str = get_next_line(fd);
+	str2 = get_next_line(fd);
 	printf("%s\n", str);
+	printf("%s\n", str2);
 	free(str);
+	free(str2);
 	return (0);
 }
