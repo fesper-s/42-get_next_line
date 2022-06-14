ff185fd7c734fedb9de4d946349f02cb9a118821
fesper-s <fafaeesfabricio@gmail.com>
2022-06-14 09:11:45 -0300

    final finish

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fesper-s <fesper-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 08:27:07 by fesper-s          #+#    #+#             */
-/*   Updated: 2022/06/13 14:22:01 by fesper-s         ###   ########.fr       */
+/*   Updated: 2022/06/14 09:07:46 by fesper-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,33 @@
 
 int	main(void)
 {
-	int		fd;
-	char	*str;
-	char	*str2;
-
-	fd = open("./teste.txt", O_RDONLY);
-	str = get_next_line(fd);
-	str2 = get_next_line(fd);
-	printf("%s", str);
-	printf("%s", str2);
-	free(str);
-	free(str2);
+	char	*line;
+	int		i;
+	int		fd1;
+	int		fd2;
+	int		fd3;
+	fd1 = open("./test1.txt", O_RDONLY);
+	fd2 = open("./test2.txt", O_RDONLY);
+	fd3 = open("./test3.txt", O_RDONLY);
+	i = 1;
+	while (i < 5)
+	{
+		line = get_next_line(fd1);
+		printf("%d --> ", fd1);
+		printf("line %02d: %s\n", i, line);
+		free(line);
+		line = get_next_line(fd2);
+		printf("%d --> ", fd2);
+		printf("line %02d: %s\n", i, line);
+		free(line);
+		line = get_next_line(fd3);
+		printf("%d --> ", fd3);
+		printf("line %02d: %s\n", i, line);
+		free(line);
+		i++;
+	}
+	close(fd1);
+	close(fd2);
+	close(fd3);
 	return (0);
 }
